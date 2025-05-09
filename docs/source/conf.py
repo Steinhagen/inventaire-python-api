eac5e583f35c8a370a57123ed35cdcced540db83
Viorel-Cătălin Răpițeanu <rapiteanu.catalin@gmail.com>
2025-05-09 15:28:21 +0300

    Update Copyright with the correct years
    Update the years and the copyright owners.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -13,9 +13,9 @@ sys.path.insert(0, os.path.abspath("../../"))
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
 project = "Inventaire Python API"
-copyright = ["2024, Viorel-Catalin Rapiteanu"]
+copyright = ["2025, Viorel-Catalin Rapiteanu"]
 author = "Viorel-Catalin Rapiteanu"
-release = "0.1.0"
+release = "0.1.1"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
